a166273774d28cba24a98cedc7dde92283f9b501
aorri2 <aorri96@naver.com>
2023-03-31 00:42:44 +0900

    [feat] : 초간단 DAO
    - 책과 달리 userName `root`로,
    - Driver 이름도 `"com.mysql.cj.jdbc.Driver"`로 다르게 함

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.user.dao.UserDao;
+import com.example.demo.user.domain.User;
+
+@SpringBootApplication
+public class DemoApplication {
+
+
+
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		UserDao dao = new UserDao();
+
+		User user = new User();
+		user.setId("wook");
+		user.setName("종욱");
+		user.setPassword("devloper");
+
+		dao.add(user);
+
+		System.out.println(user.getId() + " 등록 성공");
+
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
